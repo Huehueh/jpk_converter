d6c2d14a55eccdbf40812a20f96ad169ece06dcf
Kinga Karczewska <kinga.karczewska67@gmail.com>
2021-02-09 21:17:24 +0100

    dodane testowanie czy xml poprawny

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from xml.etree import ElementTree
 from xml.etree.ElementTree import QName, Element, SubElement, register_namespace
 import re
-import xml.dom.minidom as minidom
+from test import is_xml_correct
 
 tags = {'tnsa': 'http://crd.gov.pl/wzor/2020/05/08/9394/',
         'etd': 'http://crd.gov.pl/xml/schematy/dziedzinowe/mf/2020/03/11/eD/DefinicjeTypy/'}
@@ -9,18 +9,15 @@ tags = {'tnsa': 'http://crd.gov.pl/wzor/2020/05/08/9394/',
 for key, value in tags.items():
     register_namespace(key, value)
 
-def prettify(elem : Element):
-    rough_string = ElementTree.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="\t")
-
-
-def getTnsaElementWithTag(tag : str):
+def getTnsaElementWithTag(tag : str, document : Element):
     tnsa_tag = str(QName(tags['tnsa'], tag))
     list = document.findall(f'.//{tnsa_tag}')
     if len(list) == 0:
         return None
     return list[0]
+
+def getTnsaTag(text : str):
+    return str(QName(tags['tnsa'], text))
 
 def updateOsobaFizyczna(document : Element):
     reps = {tags['tnsa']: tags['etd']}
@@ -29,59 +26,50 @@ def updateOsobaFizyczna(document : Element):
     find_replacement = lambda m : reps[m.group(1)]
     regex = r'({})'.format(r'|'.join(re.escape(w) for w in reps))
 
-    os_fiz = getTnsaElementWithTag('OsobaFizyczna')
-    # os_fiz_tag = str(QName(tags['tnsa'], 'OsobaFizyczna'))
-    # os_fiz = document.findall(f'.//{os_fiz_tag}')
-    # if len(os_fiz) == 0:
-    #     return
-
+    os_fiz = getTnsaElementWithTag('OsobaFizyczna', document)
     if os_fiz is None:
         print("W dokumencie nie ma pola 'Osoba fizyczna'")
 
     for element in os_fiz:
-        print (element.tag, element.text)
         if element.tag in tags_to_change:
+            print(f"Zmieniam {element.tag}")
             element.tag = re.sub(regex, find_replacement, element.tag)
-        print(element.tag, element.text)
 
+def createTnsaSubElement(text : str, parent : Element):
+    tag = str(QName(tags['tnsa'], text))
+    new_element = SubElement(parent, tag)
+    new_element.text = "0"
 
 def updateZakupySprzedaze(document : Element):
     options = {'SprzedazCtrl' : ['LiczbaWierszySprzedazy', 'PodatekNalezny'],
                'ZakupCtrl' : ['LiczbaWierszyZakupow', 'PodatekNaliczony']}
 
-    ew_tag = str(QName(tags['tnsa'], 'Ewidencja'))
-    ewidencja = document.findall(f'.//{ew_tag}')
-    if len(ewidencja) < 1:
-        print("Brak elementu ewidencja")
-        return 1
-    print(ewidencja[0])
-
+    section = 'Ewidencja'
+    pole_ewidencja = getTnsaElementWithTag(section, document)
     for option in options.keys():
-        tag = str(QName(tags['tnsa'], option))
-        opt_list = document.findall(f'.//{tag}')
-        if len(opt_list) == 0:
-            print(f"NI MA {tag}")
-            newOption = SubElement(ewidencja[0], tag)
-            for sub_opt in options[option]:
-                sub_tag = str(QName(tags['tnsa'], sub_opt))
-                sub_elem = SubElement(newOption, sub_tag)
-                sub_elem.text = "0"
-                print(sub_elem.tag, sub_elem.text)
-    return 0
+        element = getTnsaElementWithTag(option, document)
+        if element == None:
+            tag = getTnsaTag(option)
+            print(f"Dodaję pole {tag} w polu Ewidencja")
+            new_element = SubElement(pole_ewidencja, tag)
+            for subelem_title in options[option]:
+                createTnsaSubElement(subelem_title, new_element)
 
 
 document = ElementTree.parse('test.XML')
 root = document.getroot()
 updateOsobaFizyczna(root)
-status = updateZakupySprzedaze(root)
+updateZakupySprzedaze(root)
 
-if status == 1:
-    print("Nie udała sie przemiana")
-
-with open('test_output.xml', 'w') as output_file:
+output_filename = 'output.xml'
+with open(output_filename, 'w') as output_file:
     output_file.write('<?xml version="1.0" encoding="utf-8"?>\n')
     output_file.write(ElementTree.tostring(root, encoding='utf-8').decode())
-    # output_file.write(prettify(root))
 
+good = is_xml_correct(output_filename)
+if good == None:
+    print("Nie można sprawdzić poprawności pliku!")
+else:
+    print("XML poprawny!" if good else "XML niepoprawny:(")
 
 
---
+++ test.py
@@ -0,0 +1,21 @@
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+def is_xml_correct(input_filename):
+    request_url = "https://e-mikrofirma.mf.gov.pl/jpk-client/api/Jpk/Upload"
+    mp_encoder = MultipartEncoder(
+        fields={
+            'jpk': (input_filename, open(input_filename, 'rb'), 'text/xml')
+        }
+    )
+    r = requests.post(
+        request_url,
+        data=mp_encoder,
+        headers={'Content-Type': mp_encoder.content_type}
+    )
+    if r.status_code != 200:
+        return None
+
+    errors_list = r.json()[0]['validationErrors']
+    # print(errors_list)
+    return (len(errors_list) == 0)
