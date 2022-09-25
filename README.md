bbcb9b9d0b2f4fafe54dcae810f0a236d97136b4
Huehueh <kinga.karczewska67@gmail.com>
2022-09-25 21:44:24 +0200

    upgrade wersji schemy

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # jpk_converter
 
-To create exe file run: "auto-py-to-exe"
+To create exe file run: "auto-py-to-exe" to diplay window or enter the command: 
+pyinstaller --noconfirm --onefile --windowed  "F:/Dokumenty/PycharmProjects/jpk_converter/main.py"
--- upgrader.py
+++ upgrader.py
@@ -1,5 +1,3 @@
-
-
 from dataclasses import dataclass
 from xml.etree.ElementTree import Element
 
@@ -10,28 +8,37 @@ WERSJA_SCHEMY = "wersjaSchemy"
 JPK_V7M = "JPK_V7M"
 JPK_V7K = "JPK_V7K"
 
-def upgradeV7K(kod_form, kod_form_dekl, wariant_form):
-    kod_form.attrib[KOD_SYSTEMOWY] = "JPK_V7K (2)"
-    kod_form_dekl.attrib[KOD_SYSTEMOWY] = "VAT-7 (16)"
-    kod_form_dekl.attrib[WERSJA_SCHEMY] = "1-0E"
-    wariant_form.text = '16'
 
-def upgradeV7M(pole_kod_form, pole_kod_form_dekl, pole_wariant_form):
-    pole_kod_form.attrib[KOD_SYSTEMOWY] = "JPK_V7M (2)"
-    pole_kod_form_dekl.attrib[KOD_SYSTEMOWY] = "VAT-7 (22)"
-    pole_kod_form_dekl.attrib[WERSJA_SCHEMY] = "1-0E"
-    pole_wariant_form.text = '22'
+
+
+def upgradeV7K(kod_form, kod_form_dekl, wariant_form_dekl):
+    kod_form.attrib[KOD_SYSTEMOWY] = "JPK_V7K (2)"
+    kod_form.attrib[WERSJA_SCHEMY] = "1-0E"
+    if kod_form_dekl is not None:
+        kod_form_dekl.attrib[KOD_SYSTEMOWY] = "VAT-7 (16)"
+        kod_form_dekl.attrib[WERSJA_SCHEMY] = "1-0E" # TODO: check
+        wariant_form_dekl.text = '16'
+
+
+def upgradeV7M(kod_form, kod_form_dekl, wariant_form_dekl):
+    kod_form.attrib[KOD_SYSTEMOWY] = "JPK_V7M (2)"
+    # TODO: check  WERSJA_SCHEMY
+    if kod_form_dekl is not None:
+        kod_form_dekl.attrib[KOD_SYSTEMOWY] = "VAT-7 (22)"
+        kod_form_dekl.attrib[WERSJA_SCHEMY] = "1-0E" # TODO: check
+        wariant_form_dekl.text = '22'
+
 
 def upgradeTo2022(document: Element, helper: NamespaceHelper):
-     # czesc ewidencyjna
+    # czesc ewidencyjna
     kod_form = helper.findTnsaElementWithTag('KodFormularza', document)
     kod_form_dekl = helper.findTnsaElementWithTag('KodFormularzaDekl', document)
-    wariant_form = helper.findTnsaElementWithTag('WariantFormularzaDekl', document)
+    wariant_form_dekl = helper.findTnsaElementWithTag('WariantFormularzaDekl', document)
+    wariant_form = helper.findTnsaElementWithTag('WariantFormularza', document)
 
     if kod_form.attrib[KOD_SYSTEMOWY] == "JPK_V7M (1)":
-        upgradeV7M(kod_form, kod_form_dekl, wariant_form)
+        upgradeV7M(kod_form, kod_form_dekl, wariant_form_dekl)
     elif kod_form.attrib[KOD_SYSTEMOWY] == "JPK_V7K (1)":
-        upgradeV7K(kod_form, kod_form_dekl, wariant_form)
+        upgradeV7K(kod_form, kod_form_dekl, wariant_form_dekl)
 
-    wariant_form = helper.findTnsaElementWithTag('WariantFormularza', document)
     wariant_form.text = '2'
